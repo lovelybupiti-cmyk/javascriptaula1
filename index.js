cbde5bd05e9a4aefe8293efb29a6b9341faa3987
unknown <mariofilho0502@digitalcollege.internal>
2026-01-21 11:54:27 -0300

    aula

---
+++ index.js
@@ -0,0 +1,25 @@
+// const email = prompt('digite seu e-mail')
+// const senha = prompt('digite sua senha')
+
+// if (email === 'queiroz.14@hotmail.com' && senha === 12345) {
+//     alert ('loging efetuado co sucesso')
+// } else {
+//     alert ('e-mail ou senha incorretos')
+// }
+
+// Qual o resultado da seguinte expressão?let total = 10 + 5 * 2 / 2 + 20;
+console.log((10 + 5 * 2) / (2 + 20))
+// Crie duas expressões que retornem NaN
+
+const number = Number("10%")
+console.log(number)
+
+// Somar a string '200' com o número 50 e retornar 250
+console.log(Number('200')+ 50)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+console.log(5)
+// Como dividir o peso por 2? 
+let numero = '60';
+let unidade = 'kg';
+let peso = numero + unidade;
